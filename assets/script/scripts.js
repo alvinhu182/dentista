24bb62deeb4ceb65c4aa6013c61ba3935c2f2c00
unknown <alvinhu@hotmail.com>
2023-03-28 18:40:31 -0300

    remoção dos botões na hora de imprimir

--- assets/script/scripts.js
+++ assets/script/scripts.js
@@ -11,6 +11,9 @@ function showPreview(input){
             preview.appendChild(novaImagem);
           }
           reader.readAsDataURL(input.files[i]);
+
+          var iptValueMultiple = document.getElementById("file-ip-3")
+          iptValueMultiple.classList.add("sumir")
         }
     }
 }
@@ -19,11 +22,13 @@ function showPreviewRaioX(event){
     if(event.target.files.length > 0){
       var src = URL.createObjectURL(event.target.files[0]);
       var preview2 = document.getElementById("file-ip-raiox-preview");
+      var iptValuePreview = document.getElementById("input-preview")
       preview2.src = src;
       preview2.style.display = "flex";
       preview2.style.width ="700px";
       preview2.style.height ="350px";
       preview2.style.margin ="25px 10px 25px"
+      iptValuePreview.classList.add("sumir")
      
     }
 }
@@ -45,3 +50,7 @@ var telefone = document.getElementById('telefone');
 var mascaraTelefone = IMask(telefone, {
   mask: '(00) 0000-0000'
 });
+
+
+
+ 
